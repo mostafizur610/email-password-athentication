169813d5818d98c850546dc74be660d8b866fad2
mostafizur610 <mostafizurrahman273@gmail.com>
2022-10-19 08:51:08 +0600

    email and password authentication

---
+++ src/Components/RegisterReactBootstrap.js
@@ -0,0 +1,78 @@
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import app from '../firebase/firebase.init';
+
+
+
+const auth = getAuth(app);
+
+const RegisterReactBootstrap = () => {
+    const [passwordError, setPasswordError] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    const handleRegister = event => {
+        event.preventDefault();
+        setSuccess(false);
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+        if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+            setPasswordError('please provide at least two uppercase');
+            return;
+        }
+
+        if (password.length < 6) {
+            setPasswordError('please should be at least 6 character');
+            return;
+        }
+
+        if (!/(?=.*[!@#$&*])/.test(password)) {
+            setPasswordError('please provide at least one special character');
+            return;
+        }
+        setPasswordError('');
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setSuccess(true);
+                form.reset();
+            })
+            .catch(error => {
+                console.error('error', error);
+                setPasswordError(error.message);
+            })
+    }
+
+    return (
+        <div className='w-50 mx-auto'>
+            <h3 className='text-primary'>PLease Register!!!</h3>
+            <Form onSubmit={handleRegister}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <p className='text-danger'>{passwordError}</p>
+                {success && <p className='text-success'>User Created successfully</p>}
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+            </Form>
+        </div>
+    );
+};
+
+export default RegisterReactBootstrap;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBeSh76CWgegeE1lP-9kQV-ckMw1nZHyYY",
+    authDomain: "email-password-auth-209da.firebaseapp.com",
+    projectId: "email-password-auth-209da",
+    storageBucket: "email-password-auth-209da.appspot.com",
+    messagingSenderId: "1096464747727",
+    appId: "1:1096464747727:web:0a9e05fcfc89b317b67bba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
